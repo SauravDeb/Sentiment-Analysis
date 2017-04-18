78ef78f4c208e7440805788f8fb29e3dfb0d91c0
Saurav Deb <Saurav Deb>
2017-04-18 23:37:12 +0530

    Addition of sentiment distribution

--- demonetization.R
+++ demonetization.R
@@ -5,26 +5,38 @@ library(tidytext)
 library(wordcloud)
 library(ggplot2)
 library(RColorBrewer)
+library(tm)
+library(SnowballC)
+
 demonetization_tweets <- read_csv("~/R works/demonetization-tweets.csv") 
+
 #imported as a tibble. Hence the 'factor' type texts are converted to char.
+
 demonet_text_tibble <- demonetization_tweets[c("X1" , "text")]
 tidy_demonet <- demonet_text_tibble %>% unnest_tokens(word, text)
+#Removing the stopwords
 tidy_demonet <- tidy_demonet %>% anti_join(stop_words)
-#Using the 'bing' lexicons
-demonet_sentiment_bing <- tidy_demonet %>% inner_join(get_sentiments("bing")) %>% count(X1, sentiment)  %>% spread(sentiment, n, fill = 0) %>% mutate(sentiment = positive - negative)
-demonet_sentiment_bing$net_sentiment <- demonet_sentiment$sentiment
-demonet_sentiment_bing$net_sentiment[demonet_sentiment$sentiment > 1] <- "Positive"
-demonet_sentiment_bing$net_sentiment[demonet_sentiment$sentiment > -1 & demonet_sentiment$sentiment <= 1] <- "Neutral"
-demonet_sentiment_bing$net_sentiment[demonet_sentiment$sentiment <= -1] <- "Negative"
-#Using the 'afinn' lexicon
+
+#Doing inner-join using the 'afinn' sentiment lexicon
 demonet_sentiment_afinn <- tidy_demonet %>% inner_join(get_sentiments("afinn")) %>%  group_by(Tweet_no. = X1) %>% summarise(sentiment = sum(score))
-#Usiing the 'nrc' lexicon
-demonet_sentiment_nrc <- tidy_demonet %>% inner_join(get_sentiments("nrc")) %>% filter(sentiment==c("positive", "negative"))
-#Plotting our sentiments
-qplot(demonet_sentiment$line, demonet_sentiment$sentiment, demonet_sentiment, geom = "line", xlab = "TWEETS", ylab = "SENTIMENT", color = 'red')
-#Plotting a bar chart
+
+#Plotting our sentiments score as compared to the no. of tweets
+ggplot(demonet_sentiment_afinn, aes(Tweet_no., sentiment, fill = 0)) +
+       geom_col(show.legend = FALSE) + labs(title = "Sentiment score Distribution", x = "Tweet No.", y = "Sentiment score")
+
+#Plotting a bar chart of positive, negative and neutral sentiments
+demonet_sentiment_afinn$score[demonet_sentiment_afinn$sentiment < 0] <- "Negative"
+demonet_sentiment_afinn$score[demonet_sentiment_afinn$sentiment == 0] <- "Neutral"
+demonet_sentiment_afinn$score[demonet_sentiment_afinn$sentiment > 0] <- "Positive"
+demonet_sentiment_afinn <- demonet_sentiment_afinn %>% count(Tweet_no., score)
 ggplot(demonet_sentiment_afinn, aes(score,n, fill = score)) + geom_bar(stat="identity", colour="black") + labs(title = "Demonetisation sentiment analysis", x = "Sentiment", y = "Tweet Count")
+
+
 #Forming wordcloud
 corpus <- Corpus(VectorSource(demonetization_tweets$text))
  #--------clean and stem-----------
+corpus <- tm_map(corpus, tolower)
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, removeWords,c("demonetisation", "demonetization", "demonet",stopwords("english")))
+corpus <- tm_map(corpus, stemDocument)
 wordcloud(corpus, max.words = 500, random.order = F, scale(4,0.5), rot.per = 0.35, colors = brewer.pal(8,"Dark2"))
